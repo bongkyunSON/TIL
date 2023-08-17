8d89f2c81ddf7cf55a8cdc50c3317948586d0fb9
bongkyunSON <BK.Son815@gmail.com>
2023-08-17 16:32:19 +0900

    refact:

---
+++ ChatGPT/Chat_GPT/Chatbot/backend.py
@@ -0,0 +1,30 @@
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+import openai
+
+openai.api_key = "<YOUR_OPENAI_API_KEY>"
+
+
+def chat(messages):
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+    resp_dict = response.to_dict_recursive()
+    assistant_turn = resp_dict['choices'][0]['message']
+    return assistant_turn # {"role": "assistant", "content": "blahblahblah"}
+
+app = FastAPI()
+
+class Turn(BaseModel):
+    role: str
+    content: str
+
+class Messages(BaseModel):
+    messages: List[Turn]  # [{"role": "user", "content": "blahblahblah"}, {"role": "assistant", "content": "blahblahblah"}, ...]
+
+
+@app.post("/chat", response_model=Turn)
+def post_chat(messages: Messages):
+    messages = messages.dict()
+    assistant_turn = chat(messages=messages['messages'])
+    return assistant_turn
--- ChatGPT/FastAPI/main.py
+++ ChatGPT/FastAPI/main.py
@@ -14,22 +14,19 @@ app.put: Update  <<<
 app.delete: Delete  <<<
 """
 items = {
-    0: {"name": "bread",
-        "price": 1000},
-    1: {"name": "water",
-        "price": 500},
-    2: {"name": "라면",
-        "price": 1200}
-}
+			0: {"name": "bread",
+				"price": 1000},
+		    1: {"name": "water",
+			    "price": 500},
+		    2: {"name": "라면",
+				"price": 1200}
+	    }
 
 # Path parameter
-
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     item = items[item_id]
     return item
-
 
 @app.get("/items/{item_id}/{key}")
 def read_item_and_key(item_id: int, key: str):
@@ -37,8 +34,6 @@ def read_item_and_key(item_id: int, key: str):
     return item
 
 # Query parameter
-
-
 @app.get("/item-by-name")
 def read_item_by_name(name: str):
     for item_id, item in items.items():
@@ -51,35 +46,31 @@ class Item(BaseModel):
     name: str
     price: int
 
-
 @app.post("/items/{item_id}")
-def create_item(item_id: int, item: Item):
+def create_item(item_id:int, item: Item):
     if item_id in items:
         return {"error": "there is already existing key."}
     items[item_id] = item.dict()
     return {"success": "ok"}
 
-
 class ItemForUpdate(BaseModel):
     name: Optional[str]
     price: Optional[int]
 
-
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: ItemForUpdate):
+def update_item(item_id:int, item: ItemForUpdate):
     if item_id not in items:
         return {"error": f"there is no item id: {item_id}"}
 
     if item.name:
-        items[item_id]['name'] = item.name
+        items[item_id]['name'] = item.name 
 
     if item.price:
-        items[item_id]['price'] = item.price
+        items[item_id]['price'] = item.price 
 
     return {"success": "ok"}
 
-
 @app.delete("/items/{item_id}")
-def delete_item(item_id: int):
+def delete_item(item_id:int):
     items.pop(item_id)
     return {"success": "ok"}
